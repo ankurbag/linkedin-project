cbf1b7a1005a094f8222a901bf4ddc661d8fd42f
Ankur Bag <bagankur@gmail.com>
2023-06-17 09:20:49 -0700

    get data from obj, add functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,76 @@
-// folder names : kebab-case-2
-// js variables : camelCase
+// Store in object
+const userProfile = {
+	username: "Ankur Bag",
+	profilePicture: "./assets/images/man.png",
+	jobTitle: "Software Engineer",
+};
 
-// Store data
-let username = "Ankur Bag";
-let profilePicture = "./assets/images/man.png";
-let jobTitle = "Software Engineer";
+// load User profile
+const loadUserProfile = () => {
+	// DOM Manipulation
+	// 1. Target the element
+	// 2. Store the element in some temp variable
+	// 3. Add / remove / Update the elemet
+	let usernameElemnts = document.querySelectorAll(".data-username");
+	for (const element of usernameElemnts) {
+		element.innerHTML = userProfile.username;
+	}
 
-// DOM Manipulation
-// 1. Target the element
-// 2. Store the element in some temp variable
-// 3. Add / remove / Update the elemet
-let usernameElemnts = document.querySelectorAll(".data-username");
-// for (let i = 0; i < usernameElemnts.length; i++) {
-// 	usernameElemnts[i].innerHTML = username;
-// }
-for (const element of usernameElemnts) {
-	element.innerHTML = username;
-}
+	let profilePictureElement = document.querySelector(".data-profile-picture");
+	profilePictureElement.src = userProfile.profilePicture;
 
-let profilePictureElement = document.querySelector(".data-profile-picture");
-profilePictureElement.src = profilePicture;
+	let jobTitleElement = document.querySelector(".data-jobtitle");
+	jobTitleElement.innerHTML = userProfile.jobTitle;
+};
 
-let jobTitleElement = document.querySelector(".data-jobtitle");
-jobTitleElement.innerHTML = jobTitle;
+// Create Post
+const createPost = () => {
+	// Get the  text from the post text box
+	let userPostText = document.querySelector(".data-post-text").value;
+	console.log(userPostText);
 
-// Create post
-// 1. Target the element
-// 2. Store the element in some temp variable
-// 3. Add / remove / Update the elemet
+	// Create a new POst component
+	/**
+	 *              <div class="posts__item">
+						<div class="posts__username">User 1 Posted</div>
+						<div class="posts__content">
+							<p>
+								userPostText
+							</p>
+						</div>
+						<div class="posts__like"><button>Like</button></div>
+					</div>
+	 */
+	let postItem = document.createElement("div");
+	postItem.classList.add("posts__item");
 
-// Create a element
-// Push / append in the container
+	let postUserName = document.createElement("div");
+	postUserName.classList.add("posts__username");
+	postUserName.innerHTML = `${userProfile.username} Posted`;
+
+	let postContent = document.createElement("div");
+	postContent.classList.add("posts__content");
+
+	let postContentPar = document.createElement("p");
+	postContentPar.innerHTML = userPostText;
+
+	let postlikeDiv = document.createElement("div");
+	postlikeDiv.classList.add("posts__like");
+
+	let postBtn = document.createElement("button");
+	postBtn.innerHTML = "Like";
+
+	// append this new post component to the post container class post
+	postItem.appendChild(postUserName);
+	postItem.appendChild(postContent);
+	postItem.appendChild(postlikeDiv);
+
+	postContent.appendChild(postContentPar);
+
+	postlikeDiv.appendChild(postBtn);
+
+	document.querySelector(".posts").prepend(postItem);
+};
+
+// Function calls
+loadUserProfile();
