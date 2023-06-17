a298238bd6a6d1233b80d4cda47c2923d8eff7e5
RichardXTrusty <ratrusty92@gmail.com>
2023-06-17 09:30:29 -0400

    Changes to Username and Job Title Script

--- scripts/index.js
+++ scripts/index.js
@@ -2,29 +2,26 @@
 // JS variables should be camelCase
 
 //Store Data
-let username = "Ankur Bag";
-let profilePicture = "./assets/image/man.png";
+let username = "Richard Trusty";
+let profilePicture = "./assests/images/man/png";
 let jobTitle = "Software Engineer";
 
 //DOM Manipulation
-// ____________________
 // 1. Target the element
 // 2. Store the element in some temporary variable
 // 3. Add or remove or update the element
 
 let usernameElement = document.querySelectorAll(".data-username");
-usernameElement.innerHTML = username;
-for (let i = 0; i, usernameElements.length; i++) {
-  usernameElements[i].innerHTML = username;
+usernameElement.innerText = username;
+
+for (let i = 0; i < usernameElement.length; i++) {
+  usernameElement[i].innerText = username;
 }
-for (const element of usernameElements) {
-  element.innerHTML = username;
-}
+
+let jobTitleElement = document.querySelectorAll(".data-jobtitle");
+jobTitleElement.innerHTML = jobTitle;
 
 let profilePictureElement = document.querySelector(".data-profile-picture");
 profilePictureElement.src = profilePicture;
 
-let jobTitleElement = document.querySelector(".data-jobtitle");
-jobTitleElement.innerHTML - jobTitle;
-
-// "Data-"is used as a industry aceepted principle to target information for HTML
+//Next step is to store this in an object and execute
